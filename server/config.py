05fae5651c3b1898aeb675cd5dc2440db584b18c
Historia Montague <historia.montague@outlook.com>
2023-07-05 09:51:36 -0600

    basic file structure

---
+++ server/config.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from sqlalchemy import MetaData
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+app.secret_key = os.environ.get('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+migrate = Migrate(app, db,render_as_batch=True)
+db.init_app(app)
+
+render_as_batch=True
+
+bcrypt = Bcrypt(app)
+
+api = Api(app)
+
+CORS(app)
