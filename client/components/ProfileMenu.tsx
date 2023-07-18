1ada1ca36c2afa3d9c171384e3e266650d952b7f
Historia Montague <historia.montague@outlook.com>
2023-07-18 15:34:45 -0600

    user logout working

--- client/components/ProfileMenu.tsx
+++ client/components/ProfileMenu.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
@@ -9,12 +9,36 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import { UserContext } from "./AuthProvider";
 import { Button } from "@mui/material";
+import { getCookie } from "@/lib/getters";
+import useSWRMutation from "swr/mutation";
+import { useRouter } from "next/navigation";
 
-const settings = ["profile", "logout"];
+const sendRequest = async (url: string) => {
+  const resp = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "X-CSRF-TOKEN": getCookie("csrf_access_token"),
+      "Content-Type": "application/json",
+    },
+  });
+  if (resp.ok) {
+    return "Logout succesful";
+  } else {
+    const error_message = await resp.json();
+    const error = new Error(error_message.error);
+    throw error;
+  }
+};
 
 const ProfileMenu = () => {
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
-  const { user } = useContext(UserContext);
+  const { user, updateUser } = useContext(UserContext);
+  const router = useRouter();
+
+  const { trigger, data, isMutating, error } = useSWRMutation(
+    "/api/v1/logout",
+    sendRequest
+  );
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
@@ -22,6 +46,14 @@ const ProfileMenu = () => {
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
+
+  useEffect(() => {
+    if (data) {
+        router.push("/");
+        updateUser(null);
+    }
+  }, [data]);
+
   return user ? (
     <Box sx={{ flexGrow: 0 }}>
       <Tooltip title="Open settings">
@@ -45,13 +77,16 @@ const ProfileMenu = () => {
         open={Boolean(anchorElUser)}
         onClose={handleCloseUserMenu}
       >
-        {settings.map((setting) => (
-          <MenuItem key={setting} onClick={handleCloseUserMenu}>
-            <Button href={setting}>
-              <Typography textAlign="center">{setting}</Typography>
-            </Button>
-          </MenuItem>
-        ))}
+        <MenuItem onClick={handleCloseUserMenu}>
+          <Button href="/profile">
+            <Typography textAlign="center">Profile</Typography>
+          </Button>
+        </MenuItem>
+        <MenuItem onClick={handleCloseUserMenu}>
+          <Button onClick={() => trigger()}>
+            <Typography textAlign="center">logout</Typography>
+          </Button>
+        </MenuItem>
       </Menu>
     </Box>
   ) : (
