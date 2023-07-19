ff602fd8aeab5a8cd19969bc38e17eef71f41d67
Historia Montague <historia.montague@outlook.com>
2023-07-19 16:31:02 -0600

    style sign up form

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,13 +1,24 @@
 "use client";
 import { UserContext } from "@/components/AuthProvider";
-import { Formik, Field, Form, ErrorMessage, connect, getIn } from "formik";
+import { useFormik } from "formik";
 import { useContext, useEffect } from "react";
 import useSWRMutation from "swr/mutation";
 import { AuthInterface } from "@/lib/types";
 import { useRouter } from "next/navigation";
 import * as Yup from "yup";
 import ErrorSnackbar from "@/components/ErrorSnackbar";
-import { Button } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 const sendRequest = async (url: string, { arg }: { arg: AuthInterface }) => {
   const resp = await fetch(url, {
@@ -57,6 +68,18 @@ const Signup = () => {
   const { updateUser } = useContext(UserContext);
   const router = useRouter();
 
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: signUpSchema,
+    onSubmit: (values) => {
+      trigger(values);
+    },
+  });
+
   useEffect(() => {
     if (data) {
       router.push("/");
@@ -65,45 +88,76 @@ const Signup = () => {
   }, [data]);
 
   return (
-    <div>
-      <h1>signup</h1>
-      <Formik
-        initialValues={{
-          username: "",
-          email: "",
-          password: "",
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
-        validationSchema={signUpSchema}
-        onSubmit={(values: AuthInterface) => trigger(values)}
       >
-          <Form>
-            <label htmlFor="username">username</label>
-            <Field id="username" name="username" placeholder="username" />
-            <ErrorMessage name="username">
-              {(error: string) => <ErrorSnackbar error={error}></ErrorSnackbar>}
-            </ErrorMessage>
-
-            <label htmlFor="email">Email</label>
-            <Field id="email" name="email" placeholder="email" type="email" />
-            <ErrorMessage name="email">
-              {(error: string) => <ErrorSnackbar error={error}></ErrorSnackbar>}
-            </ErrorMessage>
-
-            <label htmlFor="password">password</label>
-            <Field
-              id="password"
-              name="password"
-              placeholder="password"
-              type="password"
-            />
-            <ErrorMessage name="password">
-              {(error: string) => <ErrorSnackbar error={error}></ErrorSnackbar>}
-            </ErrorMessage>
-            <Button type="submit">Submit</Button>
-          </Form>
-      </Formik>
-      {error?<ErrorSnackbar error={error.message} />:null}
-    </div>
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={formik.handleSubmit}
+          noValidate
+          sx={{ mt: 1 }}
+        >
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign up
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="/signin" variant="body2">
+                {"Already have an account?"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
