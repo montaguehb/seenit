5a3dc00036e8c5e5d5d4cface19a2085b7a7978b
Historia Montague <historia.montague@outlook.com>
2023-07-20 14:22:56 -0600

    removed rewrite to localhost

--- api/index.py
+++ api/index.py
@@ -40,6 +40,10 @@ api.add_resource(UserPosts, "/userposts")
 api.add_resource(UserPostById, "/userposts/<int:id>")
 api.add_resource(Communities, "/communities")
 
+@app.route("/api/python")
+def hello_world():
+    return "<p>Hello, World!</p>"
+    
 @app.route("/api/profile", methods=["GET"])
 def profile():
     if user := db.session.get(User, id):
